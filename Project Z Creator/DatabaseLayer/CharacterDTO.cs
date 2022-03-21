8c858860ac7b6e4731bf07a87019f926c5e99406
Joshua Geven <Joshua Geven@SLEEPLESS>
2022-03-21 15:57:17 +0100

    Add project files.

---
+++ Project Z Creator/DatabaseLayer/CharacterDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Z_Creator.DatabaseLayer
+{
+    public class CharacterDTO
+    {
+        public string Name;
+        public int Cost;
+    }
+}
---
+++ Project Z Creator/LogicLayer/OccupationContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Z_Creator.DatabaseLayer;
+
+namespace Project_Z_Creator.LogicLayer
+{
+    internal class OccupationContainer
+    {
+        public List<Occupations> Occupations = new List<Occupations>();
+
+        public List<Occupations> GetOccupations()
+        {
+            OccupationDAL dal = new OccupationDAL();
+            List<OccupationDTO> list = dal.GetOccupations();
+            foreach (OccupationDTO occupations in list)
+            {
+                Occupations newoccupation = new Occupations(occupations);
+                Occupations.Add(newoccupation);
+            }
+            return Occupations;
+        }
+
+    }
+}
